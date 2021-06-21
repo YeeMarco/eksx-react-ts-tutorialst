92d80612a2059422c5ddaef1d92f978160f89eb1
xiemengyu <xiemy26@chinaunicom.cn>
2021-06-21 22:00:28 +0800

    'envent'

--- jira-react/src/views/project/project-list/index.jsx
+++ jira-react/src/views/project/project-list/index.jsx
@@ -1,29 +1,41 @@
-export const  ProjectList = (props)=>{
+export const ProjectList = (props) => {
     console.log(props)
-    let {project_list} = props
+    let { project_list } = props
     return (
         <>
             <div>ProjectList</div>
-            <table>
-                <th></th>
-                <tbody>
-                {project_list.map((project)=>{
-                return (
-                    <tr  key={project.id}>
-                      {  
-                        Object.keys(project).map((key)=>{
-                            return (
-                                <td key={key}>{project[key]}</td>
-                            )
-                        })
+            <table className="table-auto border-separate border border-green-800">
+                <thead>
+                    <tr >
+                        {
+                            Object.keys(project_list[0]).map((key) => {
+                                return (
+                                    <th className="border border-green-600" key={key}>{key}</th>
+                                )
+                            })
                         }
-                        
                     </tr>
-                )
-            })}
+
+
+                </thead>
+
+                <tbody>
+                    {project_list.map((project) => {
+                        return (
+                            <tr key={project.id}>
+                                {
+                                    Object.keys(project).map((key) => {
+                                        return (
+                                            <td key={key}>{project[key]}</td>
+                                        )
+                                    })
+                                }
+                            </tr>
+                        )
+                    })}
                 </tbody>
             </table>
-            
+
         </>
     )
 }
--- jira-react/src/views/project/search-panel/index.jsx
+++ jira-react/src/views/project/search-panel/index.jsx
@@ -1,6 +1,18 @@
 export const  ProjectSearchPanel = (props)=>{
 
+    // let {onwer, project} = props.info
+    let {changeOnwer, changeProject} = props.action
+  
     return (
-        <div>ProjectSearchPanel</div>
+        <>
+            <div>ProjectSearchPanel</div>
+            <input type="text" onInput ={changeProject}/>
+            <select name="" id=""  onChange={changeOnwer}>
+            
+                <option value ="0">eks1</option>
+                <option value="1">eks2</option>
+                <option value="2">eks3</option>
+            </select>
+        </>
     )
 }
